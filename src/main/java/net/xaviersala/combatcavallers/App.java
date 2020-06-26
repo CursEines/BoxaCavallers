24136857bc5a2fe304915e420e8ae451e82f0549
fros10 <fros10@xtec.cat>
2020-06-26 12:13:35 +0200

    Afegit nou lluitador que sempre pica al cap i es protegeix sempre el cap i una altra part

--- src/main/java/net/xaviersala/combatcavallers/App.java
+++ src/main/java/net/xaviersala/combatcavallers/App.java
@@ -1,7 +1,13 @@
 package net.xaviersala.combatcavallers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import net.xaviersala.combatcavallers.combat.Ring;
+import net.xaviersala.combatcavallers.lluitador.ILluitador;
 import net.xaviersala.combatcavallers.lluitador.LLuitadorRandom;
+import net.xaviersala.combatcavallers.lluitador.LluitadorCap;
 
 /**
  * Programa que simula el funcionament de la 'boxa entre cavallers'.
@@ -17,10 +23,23 @@ public class App {
     LLuitadorRandom lluitador1 = new LLuitadorRandom("MatxacaCaps");
     LLuitadorRandom lluitador2 = new LLuitadorRandom("DestrossaPilotes");
 
+    // Creo boxejador que sempre pica al cap
+    LluitadorCap lluitador3 = new LluitadorCap("PicaCaps");
+
     // Poso els dos boxejadors en el combat
     Ring combat = new Ring();
 
-    combat.EntradaLluitadors(lluitador1, lluitador2);
+    // Creo procés random per a designar els combatents
+    List<ILluitador> llistaLluitadors = new ArrayList<ILluitador>();
+    
+    // Omplo llista de lluitadors per a després ordenar-la aleatòriament i agafar els 2 primers que seran els qui lluitaran
+    llistaLluitadors.add(lluitador1);
+    llistaLluitadors.add(lluitador2);
+    llistaLluitadors.add(lluitador3);
+
+    Collections.shuffle(llistaLluitadors);
+    
+    combat.EntradaLluitadors(llistaLluitadors.get(0),llistaLluitadors.get(1));    
     combat.Lluiteu();
 
   }
